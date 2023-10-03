4e0a5b5bc34135cf4bd1bf32fd7d4e8e4984ec32
Mm4rek <139216867+Mm4rek@users.noreply.github.com>
2023-10-03 13:31:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function mouseOver() {
+    document.getElementById("circle").innerHTML = "You are in the circle";
+}
+
+function mouseOut() {
+    document.getElementById("circle").innerHTML = "You are outside of the circle";
+}
+
+function changeBackground() {
+    document.body.style.backgroundColor = "#808080"; 
+}
+
+function changeToBlue() {
+    document.body.style.backgroundColor = "#3498db"; 
+}
